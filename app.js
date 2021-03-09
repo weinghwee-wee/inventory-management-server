63842e8b33537589380d3460c31bce20dd19a3cc
weinghwee <weinghwee.wee@hotmail.com>
2021-03-09 15:55:41 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// require library to read environment variable from .env
+require("dotenv").config();
+
+// connect to db
+require("./src/db/mongoose");
+
+const express = require("express")
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { requestHandler } = require('./src/utils')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const route = (requestMethod, routeName, method, noAuth) => {
+  return app[requestMethod](routeName, requestHandler(method, noAuth))
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`)
+})
+
---
+++ src/utils/requestHandler.js
@@ -0,0 +1,69 @@
+// const authMiddlware = require('../middlewares/authMiddleware')
+
+const requestHandler = (method, auth) => {
+  const methodHandler = async (req, res, next) => {
+    if (req.error) {
+      return next()
+    }
+
+    try {
+      const result = await method(req, res)
+      req.result = result
+
+      next()
+    } catch (e) {
+      if (e.statusCode) {
+        req.error = {
+          statusCode: e.statusCode,
+          response: e.errorResponse
+        }
+      } else {
+        req.error = {
+          response: e
+        }
+      }
+      next()
+    }
+  }
+
+  return [
+    // authMiddlware(auth),
+    methodHandler,
+    createResponse
+  ]
+}
+
+const createResponse = (req, res, next) => {
+  const { error, result } = req
+
+  if (result) {
+    return res.send({
+      status: 200,
+      result
+    })
+  }
+
+  const { statusCode, response } = errorMapping(error)
+
+  res.status(statusCode || 400).send({
+    status: statusCode || 400,
+    error: response
+  })
+}
+
+const errorMapping = (error) => {
+  if (error.response.code && error.response.code === 11000) {
+    let key
+    const { keyValue } = error.response 
+
+    for (const property in keyValue) {
+      key = property
+    }
+    error.response = `Duplicated ${key}!`
+  }
+
+  return error
+}
+
+
+module.exports = requestHandler
